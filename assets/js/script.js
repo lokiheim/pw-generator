5d936ba6dba489fb867c73eb4568dba407398310
iain macdonald <loki@mail.com>
2023-01-15 22:57:25 +0000

    added the prompt if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,10 +88,33 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var characterLength = 8;
+var choiceArr = [];
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+    
+    characterLength = parseInt(prompt("How many characters would you like your password to be? (choose from 8 to 128 characters"));
 
+    if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+      alert("Character length needs to be numerical, 8 - 128. Please try again");
+      return false;
+    }
+    if (confirm("Would you like lower case letters in the password? click Ok if yes or click Cancel for no")) {
+      choiceArr = choiceArr.concat(lowerCasedCharacters);
+    }
+    if (confirm("Would you like upper case letters in the password? click Ok if yes or click Cancel for no")) {
+      choiceArr = choiceArr.concat(upperCasedCharacters);
+    }
+    if (confirm("Would you like special characters in the password? click Ok if yes or click Cancel for no")) {
+      choiceArr = choiceArr.concat(specialCharacters);
+    }
+    if (confirm("Would you like numbers in the password? click Ok if yes or click Cancel for no")) {
+      choiceArr = choiceArr.concat(numericCharacters);
+    }
+    return true;
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -117,4 +140,19 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
 
-alert("hello")
+// Pscode
+// =promt the user for pw Input ie pw length 8 to 128
+// =same if the want to included @ 1 of items in the 4 arrays
+// =have to valiate the Input
+// =display the pw on the text area so you can copy and paste
+
+
+
+
+
+
+// function generatePassword() {
+
+// }
+
+
